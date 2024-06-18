b0daf14224289f7d257c4b62170c35cb023f2416
minkyoung kim <minry20415@naver.com>
2024-06-18 22:04:31 +0900

    섹션2 - JPA 시작하기

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 - java 17
 - Maven
 - h2 database
+
+---
+## 학습 내용 - 노션 정리
+### [섹션2 - JPA 시작하기](https://www.notion.so/2-JPA-2b1ef47d5e27485eb22ec435f03784fb?pvs=4)
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -13,14 +14,47 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(2L);
-        member.setName("HelloB");
-        em.persist(member); // 저장
+        try {
+            /* 저장
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member); // 저장
+             */
+            Member findMember = em.find(Member.class, 1L); // 조회
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
-        tx.commit();
+            /*
+            Member 전체 조회 - JQUERY 사용(객체 지향 쿼리 언어)
+            테이블이 아닌 엔티티 객체를 대상으로 쿼리
+             */
+            List<Member> members = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1) // offset
+                    .setMaxResults(10) // fetch
+                    .getResultList();
+            for (Member member : members) {
+                System.out.println("member.name = " + member.getName());
+            }
 
-        em.close();
+            /*
+            수정
+            persist() 하지 않아도 저장됨
+            jpa 가 관리해주기 때문에 변경된 부분을 확인해서 update 처리
+             */
+            findMember.setName("HelloJPA");
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            // em.remove(findMember); // 삭제
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
