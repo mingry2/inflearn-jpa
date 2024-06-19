93f9e51167005d38c9bd6eb6b145cae16bc8ab84
minkyoung kim <minry20415@naver.com>
2024-06-19 21:59:12 +0900

    섹션3 - 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,38 +15,43 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /* 저장
+            /*
+            // 비영속
             Member member = new Member();
-            member.setId(2L);
+            member.setId(3L);
             member.setName("HelloB");
-            em.persist(member); // 저장
-             */
-            Member findMember = em.find(Member.class, 1L); // 조회
+
+            // 영속
+            em.persist(member);
+
+            // 영속성 컨텍스트 1차 캐시에 보관
+            Member findMember = em.find(Member.class, 3L);
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
+            */
 
             /*
-            Member 전체 조회 - JQUERY 사용(객체 지향 쿼리 언어)
-            테이블이 아닌 엔티티 객체를 대상으로 쿼리
-             */
-            List<Member> members = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1) // offset
-                    .setMaxResults(10) // fetch
-                    .getResultList();
-            for (Member member : members) {
-                System.out.println("member.name = " + member.getName());
-            }
+            // 동일성 보장 - 1차 캐시 덕분
+            Member findMember1 = em.find(Member.class, 3L);
+            Member findMember2 = em.find(Member.class, 3L);
+            System.out.println("result = " + (findMember1 == findMember2));
+            */
 
             /*
-            수정
-            persist() 하지 않아도 저장됨
-            jpa 가 관리해주기 때문에 변경된 부분을 확인해서 update 처리
-             */
-            findMember.setName("HelloJPA");
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            // 트랜잭션을 지원하는 쓰기 지연
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+            // 쓰기 지연 SQL 저장소에 쌓임
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=== 쓰기 지연 SQL 저장소");
+            // 쿼리
+            */
 
-            // em.remove(findMember); // 삭제
+            // 엔티티 수정
+            Member member = em.find(Member.class, 150L);
+            member.setName("수정수정");
+            System.out.println("=== 엔티티 수정");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,7 +10,16 @@ public class Member {
 	@Id
 	private Long id;
 
-//	@Column(name = "username") // DB 컬럼 네임 지정
+	// JPA는 기본생성자가 필요함 -> 동적으로 객체를 생성해야되기 때문(리플렉션)
+	public Member() {
+	}
+
+	public Member(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	//	@Column(name = "username") // DB 컬럼 네임 지정
 	private String name;
 
 	public Long getId() {
